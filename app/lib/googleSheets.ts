4ab9f7c8325f9a0d03618aa2e0b76a86c921b62c
dawlishwarren <dawlishwarren@gmail.com>
2025-06-23 21:01:51 +0100

    google api setup

--- app/lib/googleSheets.ts
+++ app/lib/googleSheets.ts
@@ -1,12 +1,12 @@
-import { google } from 'googleapis';
+// import { google } from 'googleapis';
 
-const sheets = google.sheets('v4');
+// const sheets = google.sheets('v4');
 
-const SERVICE_ENDPOINT = 'https://sheets.googleapis.com';
-const spreadSheetID = '1re2Kn_pqatODP0RTKe4WlfcrHRhsDtJiO3-nBN2tM1U';
+// const SERVICE_ENDPOINT = 'https://sheets.googleapis.com';
+// const spreadSheetID = '1re2Kn_pqatODP0RTKe4WlfcrHRhsDtJiO3-nBN2tM1U';
 
-const getSpreadSheetData = () => {
-	const response = sheets.spreadsheets.get(
-		`${SERVICE_ENDPOINT}/v4/spreadsheets/${spreadSheetID}`
-	);
-};
+// const getSpreadSheetData = () => {
+// 	const response = sheets.spreadsheets.get(
+// 		`${SERVICE_ENDPOINT}/v4/spreadsheets/${spreadSheetID}`
+// 	);
+// };
